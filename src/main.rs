615e1485fab013027a7d2781feda78bae9e8fddc
Martin Thoresen <martinhath@gmail.com>
2015-12-10 19:30:17 +0100

    Fix panic when no input file is found

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,24 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() {
-    let day = 2;
+    let day = 3;
 
-    let mut filename = "./input/input_day_".to_string();
-    let day_str = day.to_string();
-    filename.push_str(&day_str);
+    let filename = format!( "./input/input_day_{}", day);
 
-    let mut file = File::open(filename).unwrap();
-    let mut input = String::new();
-    let _ = file.read_to_string(&mut input);
+    let mut file = File::open(filename);
+
+    let input: String = if let Ok(ref mut f) = file {
+        let mut inp = String::new();
+        let _ = f.read_to_string(&mut inp);
+        inp
+    } else {
+        "".to_string()
+    };
 
     match day {
         1 => day1::day_1(input),
         2 => day2::day_2(input),
+        3 => day3::day_3(input),
         _ => ()
     }
 }
