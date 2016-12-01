451446699f20c32af46846c7228be61d22f4e2d5
Martin Hafskjold Thoresen <martinhath@gmail.com>
2016-12-01 14:36:46 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::env;
+
+mod tasks;
+
+fn main() {
+    let mut args = env::args();
+    let which_task = args.nth(1).unwrap().parse::<usize>().unwrap();
+
+    match which_task {
+        1 => tasks::task_1(),
+        _ => {}
+    }
+}
